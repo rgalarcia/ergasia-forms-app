a925cf6443d72c12d8f94d03e7f498ca096ff597
Ramon Garcia <ramongarciaalarcia@gmail.com>
2016-02-09 23:56:57 +0100

    AndroidManifest.xml .- New permissions asked for Internet connectivity.
    MainActivity.java
    1.- Continuing to program the ErgasiaUser class.
    2.- Adding Internet connection check to the app.
    
    activity_main.xml
    .- Readjustment of object margins.

--- app/src/main/java/formularios/ergasia/com/formulariosergasia/MainActivity.java
+++ app/src/main/java/formularios/ergasia/com/formulariosergasia/MainActivity.java
@@ -1,5 +1,7 @@
 package formularios.ergasia.com.formulariosergasia;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
@@ -8,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+import android.text.TextUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,46 +22,84 @@ public class MainActivity extends AppCompatActivity {
 
         //Declaring main_activity.xml objects to play with them later
         TextView message = (TextView)findViewById(R.id.textView);
-        EditText input = (EditText)findViewById(R.id.editText);
+        final EditText input = (EditText)findViewById(R.id.editText);
         Button submit = (Button)findViewById(R.id.button);
 
         //Hide input EditText and sumbit Button (they may not be useful at all)
         input.setVisibility(View.INVISIBLE);
         submit.setVisibility(View.INVISIBLE);
 
-        Telephone telf = new Telephone();
-        String numtelf = telf.number;
+        //First of all, let's check that the app user is connected to Internet
+        ConnectivityManager conManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = conManager.getActiveNetworkInfo();
 
-        if (numtelf == null)
+        if (netInfo != null && netInfo.isConnected())
         {
-            message.setText("Por favor, conecte la tarjeta SIM al móvil o desactive el modo avión y reinicie la aplicación para continuar.");
+            //Let's instantiate an object from the class ErgasiaUser
+            final ErgasiaUser user = new ErgasiaUser();
+            user.assignPhoneNumber(null);
+            String numtelf = user.returnPhoneNumber();
+
+            if (numtelf == null)
+            {
+                message.setText("Por favor, conecte la tarjeta SIM al móvil o desactive el modo avión y reinicie la aplicación para continuar.");
+            }
+            //How sad... the user's SIM card does not store its phone number
+            else if (numtelf.equals(""))
+            {
+                message.setText("Ha sido imposible obtener el número de teléfono de la SIM de su móvil. " +
+                        "Por favor, introdúzcalo para continuar:");
+
+                input.setVisibility(View.VISIBLE);
+                submit.setVisibility(View.VISIBLE);
+
+                //Setting a listener for the submit Button
+                submit.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        if (input.getText().toString() != null && TextUtils.isDigitsOnly(input.getText().toString()) == true)
+                        {
+                            user.assignPhoneNumber(input.getText().toString());
+                        }
+                    }
+                });
+
+            }
+            //Wow!! We got the telephone number from the SIM card!
+            else
+            {
+                message.setText(numtelf);
+            }
         }
-        //How sad... the user's SIM card does not store its phone number
-        else if (numtelf.equals(""))
-        {
-            message.setText("Ha sido imposible obtener el número de teléfono de la SIM de su móvil. " +
-                    "Por favor, introdúzcalo para continuar:");
-
-            input.setVisibility(View.VISIBLE);
-            submit.setVisibility(View.VISIBLE);
-
-            //Setting a listener for the submit Button
-            submit.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    // Perform action on click
-                }
-            });
-
-        }
-        //Woww!! We got the telephone number from the SIM card!
         else
         {
-            message.setText(numtelf);
+            message.setText("Para que la aplicación pueda operar correctamente, debe estar conectado a Internet (vía Wifi o red de datos). " +
+                    "Por favor, conéctese a Internet y reinicie la aplicación.");
         }
+
     }
 
-    class Telephone {
-        TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-        String number = tm.getLine1Number();
+    public class ErgasiaUser {
+
+        private String phonenum;
+        private int status;
+        private int form;
+
+        public void assignPhoneNumber(String inputnum) {
+
+            if (inputnum == null) {
+                TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                phonenum = tm.getLine1Number();
+            }
+            else
+            {
+                phonenum = inputnum;
+            }
+
+        }
+
+        public String returnPhoneNumber() {
+            return phonenum;
+        }
+
     }
 }
